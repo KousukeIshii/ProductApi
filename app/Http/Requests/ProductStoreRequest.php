f56c18c3ef3784c319faf4a81accbfc44e629d20
石井康介 <faitayahhuu@yahoo.co.jp>
2019-07-25 00:20:11 +0900

    画像ファイルであるかのバリデーションを追加

--- app/Http/Requests/ProductStoreRequest.php
+++ app/Http/Requests/ProductStoreRequest.php
@@ -38,6 +38,21 @@ class ProductStoreRequest extends FormRequest
             if ( base64_encode(base64_decode($this->input('image'), true)) !== $this->input('image')) {
                 $validator->errors()->add('image', 'Image must be encoded with BASE64');
             }
+            $img = base64_decode($this->input('image'));
+            $type = finfo_buffer(finfo_open(), $img,FILEINFO_MIME_TYPE);
+            switch ($type) {
+                case 'image/jpeg':
+                    $ext='jpg';
+                    break;
+                case 'image/png':
+                    $ext='png';
+                    break;
+                case 'image/gif':
+                    $ext='gif';
+                    break;
+                default:
+                    $validator->errors()->add('image', 'extension must be jpg/png/gif');
+            }
         });
     }
 
