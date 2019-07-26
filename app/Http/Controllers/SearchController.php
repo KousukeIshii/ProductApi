906214c3d37e3bcd035b0a3646b0dfe1359fd87e
石井康介 <faitayahhuu@yahoo.co.jp>
2019-07-26 23:45:09 +0900

    SearchController作成

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\product;
+use Illuminate\Http\Request;
+use App\Http\Requests\SearchRequest;
+
+class SearchController extends Controller
+{
+    public function search(SearchRequest $request){
+
+    }
+}
